6622a0ea305890b19848b9ec916c9bdf3a37c50b
rgomez <rafael.gomez@code.seat>
2023-12-20 21:57:52 +0100

    remove konsist failure

--- src/main/kotlin/rafa/gomez/videogametrivia/challenge/application/search/ChallengeSearcher.kt
+++ src/main/kotlin/rafa/gomez/videogametrivia/challenge/application/search/ChallengeSearcher.kt
@@ -2,16 +2,14 @@ package rafa.gomez.videogametrivia.challenge.application.search
 
 import rafa.gomez.videogametrivia.challenge.domain.Category
 import rafa.gomez.videogametrivia.challenge.domain.Challenge
-import rafa.gomez.videogametrivia.challenge.domain.SearchChallengeCriteria.ByCategoryAndDifficulty
 import rafa.gomez.videogametrivia.challenge.domain.ChallengeRepository
+import rafa.gomez.videogametrivia.challenge.domain.SearchChallengeCriteria.ByCategoryAndDifficulty
 import rafa.gomez.videogametrivia.question.domain.Difficulty
 
 class ChallengeSearcher(private val repository: ChallengeRepository) {
 
     suspend operator fun invoke(category: Category, difficulty: Difficulty): List<Challenge> =
         repository.search(ByCategoryAndDifficulty(category, difficulty))
-
-    suspend fun invoke2(): Nothing = TODO()
 }
 
 sealed interface SearchChallengeError {
